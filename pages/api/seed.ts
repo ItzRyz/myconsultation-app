3c85f57b9ac18d303bf40855bb7ff337c0cd0a08
Al Farizi <74163386+ItzRyz@users.noreply.github.com>
2023-08-29 19:26:00 +0700

    Update seed.ts

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,27 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import bcryptjs from "bcryptjs";
 import User from "@/models/User";
 import db from "@/utils/Database";
 import type { NextApiRequest, NextApiResponse } from "next";
+import Admin from "@/models/Admin";
+import { createRouter } from "next-connect";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.post(async (req, res) => {
   await db.connect();
   const newUser = new User({
     username: "testuser",
     email: "testuser@gmail.com",
     password: bcryptjs.hashSync("testpw123"),
   });
-  const user = newUser.save();
+  const newAdmin = new Admin({
+    username: "testadmin",
+    email: "testadmin@gmail.com",
+    password: bcryptjs.hashSync("testpw123"),
+  });
+  // const user = newUser.save();
+  const admin = newAdmin.save();
   db.disconnect();
   res.send({
-    _id: user._id,
-    username: user.username,
-    email: user.email,
-    password: user.password,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt,
+    _id: admin._id,
+    username: admin.username,
+    email: admin.email,
+    password: admin.password,
+    createdAt: admin.createdAt,
+    updatedAt: admin.updatedAt,
   });
-}
+});
+
+export default router.handler();
